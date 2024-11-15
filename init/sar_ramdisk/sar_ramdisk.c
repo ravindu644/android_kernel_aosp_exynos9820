fc1c8d2f7d166e067480ea59dd80b8a77e535b4f
Tim Zimmermann <tim@linux4.de>
2024-11-15 20:10:05 +0100

    init: sar_ramdisk: Use kvmalloc/kvfree
    * kmalloc is the normal method of allocating memory for objects smaller than page size in the kernel.
    
    Change-Id: Id17eaac195b65c025a2a8dd2cb095ef6f872d091

--- init/sar_ramdisk/sar_ramdisk.c
+++ init/sar_ramdisk/sar_ramdisk.c
@@ -52,7 +52,7 @@ __init int mount_sar_ramdisk(char* name) {
 		goto clean_nobuf;
 	}
 
-	buf = kmalloc(header.ramdisk_size, GFP_KERNEL);
+	buf = kvmalloc(header.ramdisk_size, GFP_KERNEL);
 
 	if (!buf) {
 		pr_err("SAR_RD: Out of memory");
@@ -70,7 +70,7 @@ __init int mount_sar_ramdisk(char* name) {
 	load_default_modules();
 
 clean:
-	kfree(buf);
+	kvfree(buf);
 clean_nobuf:
 	sys_close(fd);
 
